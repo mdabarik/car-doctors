daf55ba57da182e3ff1356eca0b937dcbcb91db8
mdabarik <mdabarik19@gmail.com>
2023-10-25 11:34:33 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Car Doctor App
+
+- [Live Link](https://github.com/mdabarik/car-doctors)
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "./Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            This is home page
+        </div>
+    );
+};
+
+export default Home;
