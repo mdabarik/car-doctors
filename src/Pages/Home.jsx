0f25c91e24250d010b1aa0fea8400ff3394f7360
mdabarik <mdabarik19@gmail.com>
2023-10-25 17:57:50 +0600

    service section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import About from "./About";
 import Banner from "./Banner/Banner";
+import Services from "./Services";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/Pages/Services.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Service from "./Service";
+
+const Services = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => {
+            setServices(data)
+            console.log(data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, []);
+
+    return (
+        <div>
+            <div className="text-center space-y-3">
+                <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
+                <h2 className="text-4xl font-bold">Our Services Area</h2>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
+                <p>Services: {services.length}</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
+                    
+                    {
+                        services.map(service => <Service key={service._id} service={service}></Service>)
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
